1735d8474b8c42f62cc5bd4e4915c22a23f0bbb6
Maya Jawad Espino <mayajespino@gmail.com>
2023-06-26 12:47:13 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by maya on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+ 
+        VStack {
+            
+        }
+
+        
+    }
+}
+
